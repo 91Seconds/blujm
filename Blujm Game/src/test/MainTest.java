9d4d73dcfbc4877d6d85d90be7af46526a7176ce
Dylan Chong <dylan.chong@icloud.com>
2016-04-24 12:41:33 +1200

    Created Intellij project with a main class and test file (just for testing JUnit), setup build configurations. Added a .gitignore

---
+++ Blujm Game/src/test/MainTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.rules.ErrorCollector;
+
+/**
+ * Main Tester.
+ *
+ * @author <Authors name>
+ * @since <pre>Apr 24, 2016</pre>
+ * @version 1.0
+ */
+public class MainTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     *
+     * Method: main(String[] args)
+     *
+     */
+    @Test
+    public void testMain() throws Exception {
+
+    }
+
+
+} 
