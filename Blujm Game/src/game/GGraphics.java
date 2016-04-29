421851a7a80a893dedd238fba0de5852e8154b20
root <root@devolved-assembly>
2016-04-29 22:25:25 +1200

    asdfoasdoifj

--- Blujm Game/src/game/GGraphics.java
+++ Blujm Game/src/game/GGraphics.java
@@ -3,14 +3,22 @@ package game;
 /**
  * Created by Dylan on 29/04/16.
  */
+import ecs100.*;
+
 public class GGraphics {
     GWorld world;
+
+    int cellSize = 20;
 
     public GGraphics(GWorld world) {
         this.world = world;
     }
 
     public void draw() {
-        
+        for(int i = 0; i < world.cells.length; i++) {
+            for(int j = 0; j < world.cells[0].length; j++) {
+                UI.drawImage(world.cells[i][j].imageFile, j*cellSize, i*cellSize);
+            }
+        }
     }
 }
--- Blujm Game/src/game/GLogic.java
+++ Blujm Game/src/game/GLogic.java
@@ -18,7 +18,7 @@ public class GLogic {
     }
 
     update() {
-        
+
     }
 
     setupKeys() {
