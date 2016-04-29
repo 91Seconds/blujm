a90aabad683cb6071f25b0202fcb1ea67a52ae94
Daniel Young <Daniel Young>
2016-04-29 23:44:47 +1200

    Some basic world reading stuff

--- Blujm Game/src/game/GWorldLoader.java
+++ Blujm Game/src/game/GWorldLoader.java
@@ -1,5 +1,11 @@
 package game;
 
+import ecs100.UI;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by Dylan on 29/04/16.
  *
@@ -7,4 +13,20 @@ package game;
  */
 public class GWorldLoader {
 
+    public GWorldLoader(){
+
+    }
+
+    /**
+     * Reads a world from a file
+     * @return
+     */
+    public GWorld getWorld(int level){
+        try{
+            Scanner scan = new Scanner(new File("level" + level));
+        }
+        catch(IOException e){
+            UI.println("File Reading Error:" + e);
+        }
+    }
 }
