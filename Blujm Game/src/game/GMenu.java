b2a3ad2d8eedf6afaf426998a10c9931ae7e315f
Te Rahui Tunua <Te Rahui Tunua>
2016-04-30 15:57:30 +1200

    chaged menu to gmenu

---
+++ Blujm Game/src/game/GMenu.java
@@ -0,0 +1,12 @@
+package game;
+
+/**
+ * Created by TeRahui on 30/04/2016.
+ */
+public class GMenu {
+    private Button[] buttons;
+}
+
+
+ 
+
--- Blujm Game/src/game/Menu.java
+++
@@ -1,8 +0,0 @@
-package game;
-
-/**
- * Created by Daniel Young on 4/30/2016.
- */
-public class Menu {
-    private Button[] buttons;
-}
