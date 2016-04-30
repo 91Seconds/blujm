353aee96c2bcd2d834f05e1d01f022dd3db1bb9a
Dylan Chong <dylan.chong@icloud.com>
2016-04-30 20:03:32 +1200

    Wrote a test for the setValuesInRect in GGoal

--- Blujm Game/src/test/game/GGoalTest.java
+++ Blujm Game/src/test/game/GGoalTest.java
@@ -16,6 +16,29 @@ import org.junit.After;
 public class GGoalTest {
 
     GGoal gGoal;
+    String idealResult = "" +
+            "1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1\n";
 
     private void setSomeValues(GGoal gg) {
         gg.setValue(true, 0, 0);
@@ -23,10 +46,17 @@ public class GGoalTest {
         gg.setValue(true, 17, 21);
     }
 
+    private GGoal getNewGGoal() {
+        return new GGoal(22, 18);
+    }
+
+
     @Before
     public void before() throws Exception {
-        gGoal = new GGoal(22, 18);
+        gGoal = getNewGGoal();
         setSomeValues(gGoal);
+
+        // This bit adds the values in a rectangle manually
         for (int row = 5; row < 10; row++) {
             for (int col = 5; col < 10; col++) {
                 gGoal.setValue(true, row, col);
@@ -46,31 +76,16 @@ public class GGoalTest {
     @Test
     public void testToString() throws Exception {
         String result = gGoal.toString();
-        String idealResult = "" +
-                "1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1\n";
+
         Assert.assertEquals("Result was not equal to the idealResult", idealResult, result);
     }
 
-    @Test 
+    @Test
+    public void testSetValuesInRect() throws Exception {
+        gGoal = getNewGGoal();
+        setSomeValues(gGoal);
+        gGoal.setValuesInRect(true, 5, 5, 10, 10);
+        String result = gGoal.toString();
+        Assert.assertEquals("Result was not equal to the idealResult", idealResult, result);
+    }
 } 
