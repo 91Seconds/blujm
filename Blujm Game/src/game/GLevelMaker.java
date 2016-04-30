5ed28be9e914d0e92996fce01ab41ee59e775cd8
91Seconds <king.ciaran72@gmail.com>
2016-04-30 17:25:45 +1200

    redid the levelmaker class

--- Blujm Game/src/game/GLevelMaker.java
+++ Blujm Game/src/game/GLevelMaker.java
@@ -27,8 +27,12 @@ public class GLevelMaker {
         try {
             Scanner sc = new Scanner(level);
             for(int i=0;i<25;i++)   {
+                String[] line = sc.nextLine().split(" ");
                 for(int j=0;j<25;j++)   {
-                    System.out.println(sc.nextLine());
+                    System.out.println(line[j]);
+                    if(line[j].equals("x")) {
+                   //     GCell ngc = new GCell("")
+                    }
                 }
             }
 
