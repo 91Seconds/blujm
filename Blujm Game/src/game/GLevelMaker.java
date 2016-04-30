c2d205d05b30c8bf15f34b27b5b7a69ed9769d7b
91Seconds <king.ciaran72@gmail.com>
2016-04-30 15:31:55 +1200

    jkbjlverfgws

--- Blujm Game/src/game/GLevelMaker.java
+++ Blujm Game/src/game/GLevelMaker.java
@@ -1,11 +1,12 @@
 package game;
 
-import ecs100.UI;
-import ecs100.UIFileChooser;
+import ecs100.*;
+import ecs100.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 /**
  * Created by surface on 30/04/2016.
@@ -13,7 +14,7 @@ import java.util.Scanner;
 public class GLevelMaker {
     public static GWorld world;
 
-    public static void main(){
+    public static void main(String[] args){
         UI.initialise();
         UI.addButton("Load World from txt",GLevelMaker::initparse);
     }
@@ -25,6 +26,11 @@ public class GLevelMaker {
         GCell[][] cellArray = new GCell[25][25];
         try {
             Scanner sc = new Scanner(level);
+            for(int i=0;i<25;i++)   {
+                for(int j=0;j<25;j++)   {
+                    System.out.println(sc.nextLine());
+                }
+            }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
