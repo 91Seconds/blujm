5b1690998fa6d21ddc09092ddc65f44c37132615
root <bohannambe@myvuw.ac.nz>
2016-04-30 18:41:06 +1200

    cleaning up for pull

--- Blujm Game/src/game/GSquare.java
+++ Blujm Game/src/game/GSquare.java
@@ -1,11 +1,19 @@
 package game;
 
+import java.lang.String;
+import java.io.File;
 import java.io.Serializable;
 
 /**
  * Created by Daniel Young on 4/29/2016.
  */
 public abstract class GSquare implements Serializable{
+
+    protected static final String userType = "user";
+    protected static final String wallType = "wall";
+
+    protected static final String userPath = "live-cell.png";
+    protected static final String wallPath = "wall.png";
 
     private String imagePath;
     private String type;
