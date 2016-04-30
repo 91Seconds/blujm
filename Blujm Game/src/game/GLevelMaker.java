9b6482aded796c76c8e4221efa67c3d9eb455b2b
Dylan Chong <dylan.chong@icloud.com>
2016-05-01 10:05:36 +1200

    Changed the GGoal setValuesInRect to follow other general rect functions (like drawRect)

--- Blujm Game/src/game/GLevelMaker.java
+++ Blujm Game/src/game/GLevelMaker.java
@@ -65,7 +65,7 @@ public class GLevelMaker {
         }
 
         GGoal levelGoal = new GGoal(25,25);
-        levelGoal.setValuesInRect(true,8,1,11,4);
+        levelGoal.setValuesInRect(true,1,8,3,3);
 // TODO ciaran make the levelgoal load from the file (use the levelGoal.toString() to get the string for pasting into the file)
 
         GWorld GW = new GWorld(cellArray,levelGoal);
