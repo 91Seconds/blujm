f342fd5ddb23dd9a34d44efd58a210eeab38f73a
Daniel Young <Daniel Young>
2016-04-30 15:35:26 +1200

    FIxed compile issues

--- Blujm Game/src/game/GCell.java
+++ Blujm Game/src/game/GCell.java
@@ -8,20 +8,9 @@ import java.awt.*;
 public class GCell extends GSquare {
 
 
-    private String type;
 
     public GCell(String imageFile, String type){
-        super(imageFile);
-        this.type = type;
-    }
-
-
-
-    public String getType(){
-        return type;
-    }
-    public void setType(String newType){
-        type = newType;
+        super(imageFile, type);
     }
 
 }
